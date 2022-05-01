f8b77501e03aa443d3938b60799d99b151cff8eb
Ali Mert Özhayta <ozhayta@zoho.com>
2022-05-01 15:53:51 +0300

    Initial Commit

---
+++ SmartHomeThermostat/SmartHomeThermostatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeThermostatApp.swift
+//  SmartHomeThermostat
+//
+//  Created by Ali Mert Özhayta on 1.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeThermostatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
